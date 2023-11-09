ec3b6fd000f6b42d15cc9c7754184c3bfbdaf22f
clancywoodburn <woodburn.clancy@gmail.com>
2023-11-09 12:57:21 +1100

    Fix data reading

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ function getParameterByName(name) {
     name = name.replace(/[\[\]]/g, '\\$&');
     var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
         results = regex.exec(url);
+	console.log(results);
     if (!results) return null;
     if (!results[2]) return '';
     return decodeURIComponent(results[2].replace(/\+/g, ' '));
@@ -10,11 +11,11 @@ function getParameterByName(name) {
 
 function handle(d) {
   $(document).ready(function(){
-
+	console.log(JSON.parse(d));
     var app = new Vue({
       el: '#app',
       data: {
-        a: d[0],
+        a: JSON.parse(d),
         word: getParameterByName("x")
       }
     });
